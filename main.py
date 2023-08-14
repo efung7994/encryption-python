71bd567a24bd886d3a603b0f9fd47d9142fd19bb
Eugene <eugenefung79@gmail.com>
2023-08-14 19:25:43 -0400

    create encoding function

---
+++ main.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+
+def encrypt(text, shift):
+  message = []
+  length = len(alphabet)
+  for letter in text:
+    if letter in alphabet:
+      position = ((alphabet.index(letter) +shift) % (length))
+      message.append(alphabet[position])
+  print("".join(message))
+
+encrypt(text, shift)
