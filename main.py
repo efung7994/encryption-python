de0d0ea0cb4ac20e73e9dbc62b4a23c537816ff5
Eugene <eugenefung79@gmail.com>
2023-08-15 12:03:38 -0400

    add user experience

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import art
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(art.logo)
+
+program = True
+
+while program == True:
+
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
 
-def caesar(text, shift):
-  message = []
-  length = len(alphabet)
-  for letter in text:
-    if letter in alphabet:
-      if direction == "encode":
+  def caesar(text, shift):
+    message = []
+    length = len(alphabet)
+    if direction == "decode":
+      shift *= -1
+    for letter in text:
+      if letter not in alphabet:
+        message.append(letter)
+      else:
         position = ((alphabet.index(letter) + shift) % (length))
-      elif direction == "decode":
-        position = ((alphabet.index(letter) - shift) % (length))
-      message.append(alphabet[position])
-  print("".join(message))
+        message.append(alphabet[position])
+    print("".join(message))
 
-caesar(text=text, shift=shift)
+  caesar(text=text, shift=shift)
+
+  restart = input("Restart program? Y/N \n")
+  if restart.lower() == "n":
+    program = False
 
